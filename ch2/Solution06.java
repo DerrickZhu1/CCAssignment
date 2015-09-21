0a266deaec098fb3379bc545aae6befc61e4c0ed
DerrickZhu1 <derrickzhu1@gmail.com>
2015-09-20 20:47:31 -0400

    Update Solution06.java

--- ch2/Solution06.java
+++ ch2/Solution06.java
@@ -4,14 +4,14 @@ import java.util.Stack;
 //Accepted
 //Implement a function to check if a linked list is palindrome
 public class Solution06 {
-	public class ListNode {
+	public static class ListNode {
 		int val;
 		ListNode next;
 		ListNode(int a) {
 		    val = a;
 		}
 	}
-	public boolean isPalindrome(ListNode head) {
+	public static boolean isPalindrome(ListNode head) {
 		ListNode fast = head;
 		ListNode slow = head;
 		Stack<Integer> stack = new Stack<Integer>();
@@ -33,6 +33,18 @@ public class Solution06 {
 			slow = slow.next;
 		}
 		return true;
+	}
+       public static void main(String[] args) {
+        ListNode l1 = new ListNode(0);
+    	ListNode l2 = new ListNode(1);
+    	l1.next = l2;
+    	ListNode l3 = new ListNode(2);
+    	l2.next = l3;
+    	ListNode l4 = new ListNode(1);
+    	l3.next = l4;
+    	ListNode l5 = new ListNode(0);
+    	l4.next = l5;
+    	System.out.println(isPalindrome(l1));
 	}
 
 }
